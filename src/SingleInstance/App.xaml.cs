f30b1f1529d0370808cd3b2b7eec810692449573
SunTai（三台） <a44281071@sina.com>
2017-03-14 13:54:10 +0800

    rename UNIQUE_KEY

--- src/SingleInstance/App.xaml.cs
+++ src/SingleInstance/App.xaml.cs
@@ -6,7 +6,7 @@ namespace SingleInstance
 {
   public partial class App : Application
   {
-    private const string UNQUE_KEY = "{124D19DC-50AC-4134-84A7-8432E2CA6E8E}";
+    private const string UNIQUE_KEY = "{124D19DC-50AC-4134-84A7-8432E2CA6E8E}";
     static SimpleSingleInstance _SimpleSingleInstance;
 
     public App()
@@ -18,7 +18,7 @@ namespace SingleInstance
     private void EnsureSingleInstance()
     {
       _SimpleSingleInstance 
-        = new SimpleSingleInstance(UNQUE_KEY, OnEventWaitHandleWaitOne);
+        = new SimpleSingleInstance(UNIQUE_KEY, OnEventWaitHandleWaitOne);
       if (!_SimpleSingleInstance.IsOwned)
       {
         Shutdown();
