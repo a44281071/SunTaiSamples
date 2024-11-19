490c44eea6ae49480c4331753ebffccb1b0f47fa
SamTi（三台） <a44281071@sina.com>
2024-11-19 18:00:30 +0800

    TestGrpcShare add proto files.

---
+++ src/TestGrpcShare/Class1.cs
@@ -0,0 +1,13 @@
+﻿using GrpcGreeter;
+using System;
+
+namespace TestGrpcShare
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            var a = new HelloRequest();
+        }
+    }
+}
